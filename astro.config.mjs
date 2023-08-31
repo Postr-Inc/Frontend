144cf1cdb4034d8a6cdaa8198191508793d5d008
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-31 13:58:22 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({

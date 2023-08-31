193461664205e9e1c0fbebd38bede7e2127380cb
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-31 14:25:30 -0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
-import cloudflare from "@astrojs/cloudflare";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: cloudflare()
 });

6ca73019bc2327d0693cc386c9aa575d75fd88bc
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-01 09:11:48 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: 'https://v6-0.pages.dev/',
   integrations: [react(), tailwind()],
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare()
 });

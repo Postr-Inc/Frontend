7df7016e846bdbf4c6f8d73b6785565414b06909
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-04 16:12:38 -0700

    Update Home.jsx

--- src/react_pages/Home.jsx
+++ src/react_pages/Home.jsx
@@ -4,17 +4,24 @@ import Login from "./Login";
 import Home from "./Home";
 export const api = new Pocketbase('https://postrapi.pockethost.io')
 import { useEffect, useState } from "react";
+let init = false
 export default function App() {
+	const [isTokenFound, setTokenFound] = useState(false);
+	 
+
 	useEffect(() => {
 		if (window.matchMedia("(display-mode: browser)").matches) {
 			window.location.href = "/download"
 			
-		}else{
-			api.authStore.isValid  ? api.collection("users").authRefresh() :  console.log('not logged in')
 		}
 		
-	 
-		 
+		if(api.authStore.isValid){
+			api.collection("users").authRefresh()
+			 
+
+			 
+		} 
+		
 	}, [])
  
  

5d9ac758ba0a5ddc94dffb429f69fa9fa01bcdb8
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-05 08:06:59 -0500

    Update index.jsx

--- src/react_pages/index.jsx
+++ src/react_pages/index.jsx
@@ -13,13 +13,7 @@ export default function App() {
 		if(api.authStore.isValid){
 			api.collection("users").authRefresh()
 			 
-OneSignal.init({
-      appId: "b1beca0d-bf7b-4767-9637-7e345fff7710",
-    }).then(()=>{
-      OneSignal.login(api.authStore.model.id)
-      console.log('loggedin')
-    })
-		
+ 
 			 
 		}else if (window.matchMedia("(display-mode: browser)").matches) {
 			 window.location.href = "/download"

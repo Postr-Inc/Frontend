45c1b864bffcad32259e0cd52ff3b6e98b67f6bf
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-09 08:30:43 -0700

    Added follow notifications

--- src/react_pages/Noti.jsx
+++ src/react_pages/Noti.jsx
@@ -50,6 +50,8 @@ export default function Noti() {
     return self.findIndex((n) => {
       if(n.expand && n.expand.post && noti.expand && noti.expand.post && n.expand.author && noti.expand.author){
         return n.expand.author && n.expand.author.id === noti.expand.author.id && n.type === noti.type && n.expand.post && n.expand.post.id === noti.expand.post.id
+      }else if(n.expand  && noti.expand  && n.expand.author && noti.expand.author){
+        return n.expand.author && n.expand.author.id === noti.expand.author.id && n.type === noti.type
       }
 
     }) === index;
@@ -145,7 +147,11 @@ export default function Noti() {
 </svg> : noti.type === 'comment_like' ? 
 <svg xmlns="http://www.w3.org/2000/svg" fill="#F13B38" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#F13B38" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg> : noti.type === 'follow' ?
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-sky-500 w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
 </svg>
+
 
 
               
@@ -154,7 +160,7 @@ export default function Noti() {
               
              {
                noti.type === "like" ? <span className="text-sm">liked your post</span> : noti.type === "comment" ? <span className="text-sm">commented on your post</span> : <span className="text-sm">mentioned you in a comment</span>
-               || noti.type === "comment_like" ? <span className="text-sm">{noti.title}</span> : ""
+               || noti.type === "comment_like" ? <span className="text-sm">{noti.title}</span>  : noti.type === "follow" ? <span className="text-sm">followed you</span> : ""
              }
 
              {
@@ -192,7 +198,7 @@ export default function Noti() {
               }}
               ref={(el)=>{
                 if(el && noti.expand && noti.expand.post){
-                  el.innerHTML = noti.expand.post.content || noti.body
+                  el.innerHTML = noti.expand.post.content || noti.notification_body
                 }
               }}
               ></span>

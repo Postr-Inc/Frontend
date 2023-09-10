b3ac6d1e07679d9b4deac93d8677331c8d406138
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-09 17:48:54 -0700

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,11 @@
 import { api } from "../react_pages";
 import Modal from "./Modal";
 import { useState } from "react";
+import Filter from "bad-words";
 export default function Post(props) {
   let [likes, setLikes] = useState(props.likes);
   let [hidden, setHidden] = useState(false);
+  let [bookmarked, setBookmarked] = useState(props.bookmarked || []);
   function likepost() {
     if (likes.includes(api.authStore.model.id)) {
       let index = likes.indexOf(api.authStore.model.id);
@@ -17,32 +19,35 @@ export default function Post(props) {
       api.collection("posts").update(props.id, {
         likes: [...likes, api.authStore.model.id],
       });
-      if(props.author.id !== api.authStore.model.id){
-        console.log("creating notification")
-        api.collection("notifications").create( {
-          "image":`https://postrapi.pockethost.io/api/files/_pb_users_auth_/${api.authStore.model.id}/${api.authStore.model.avatar}` ,
-          "author": api.authStore.model.id,
-          "recipient": props.author.id,
-          "post":  props.id,
-          "title":  `${api.authStore.model.username} liked your post!`,
-          "notification_title":   `${api.authStore.model.username} liked your post!`,
-          "notification_body": `Open Postr to view this notification`,
-          "type":  "like",
-          "url": "/p/" + props.id,
-        })
+      if (props.author.id !== api.authStore.model.id) {
+        console.log("creating notification");
+        api.collection("notifications").create({
+          image: `https://postrapi.pockethost.io/api/files/_pb_users_auth_/${api.authStore.model.id}/${api.authStore.model.avatar}`,
+          author: api.authStore.model.id,
+          recipient: props.author.id,
+          post: props.id,
+          title: `${api.authStore.model.username} liked your post!`,
+          notification_title: `${api.authStore.model.username} liked your post!`,
+          notification_body: `Open Postr to view this notification`,
+          type: "like",
+          url: "/p/" + props.id,
+        });
       }
     }
   }
+
   return (
-    <div className="flex flex-col  text-sm mb-[35px]  "
-    id={props.id}
-    onClick={(e) => {
-      if(window.location.pathname !== "/p/" + props.id
-       && e.target.id === props.id
-      ){
-        window.location.href = "/p/" + props.id
-      }
-    }}
+    <div
+      className="flex flex-col  text-sm mb-[35px]  "
+      id={props.id}
+      onClick={(e) => {
+        if (
+          window.location.pathname !== "/p/" + props.id &&
+          e.target.id === props.id
+        ) {
+          window.location.href = "/p/" + props.id;
+        }
+      }}
     >
       <div className="flex flex-row ">
         {props.author.avatar ? (
@@ -95,7 +100,6 @@ export default function Post(props) {
             tabIndex="0"
             className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
           >
-           
             {props.author.id !== api.authStore.model.id ? (
               <li>
                 <a className="cursor-pointer">Report</a>
@@ -139,29 +143,33 @@ export default function Post(props) {
 
       <p
         className="mt-6 text-sm max-w-full break-words"
-         
         ref={(el) => {
           if (el) {
-            el.innerHTML = props.content;
+            localStorage.getItem("profanity") === "true"
+              ? (el.innerHTML = String(props.content).replace(
+                  new Filter().list,
+                  function (match) {
+                    return "*".repeat(match.length);
+                  }
+                ))
+              : (el.innerHTML = props.content);
           }
         }}
       ></p>
 
       {props.file ? (
         <>
-          <div
-            
-          >
+          <div>
             <img
               src={`https://postrapi.pockethost.io/api/files/w5qr8xrcpxalcx6/${props.id}/${props.file}`}
               className="w-full h-96 object-cover rounded-md mt-5 cursor-pointer"
               alt="post image"
               onClick={() => {
-                 try {
+                try {
                   document.getElementById("modal" + props.id).showModal();
-                 } catch (error) {
-                  console.log(error)
-                 }
+                } catch (error) {
+                  console.log(error);
+                }
               }}
             />
           </div>
@@ -230,7 +238,6 @@ export default function Post(props) {
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
             />
           </svg>
-           
         </div>
 
         {
@@ -250,9 +257,8 @@ export default function Post(props) {
             stroke="currentColor"
             className="w-5 h-5 cursor-pointer "
             onClick={() => {
-              if(window.location.pathname !== "/p/" + props.id){
-                window.location.href = "/p/" + props.id
-                 
+              if (window.location.pathname !== "/p/" + props.id) {
+                window.location.href = "/p/" + props.id;
               }
             }}
           >
@@ -262,9 +268,7 @@ export default function Post(props) {
               d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
             />
           </svg>
-           
         </div>
-        
 
         {
           /**
@@ -273,11 +277,15 @@ export default function Post(props) {
            * @description: repost icon
            */ ""
         }
-           
 
-           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer w-5 h-5"
-           
-           onClick={() => {
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="cursor-pointer w-5 h-5"
+          onClick={() => {
             navigator.share({
               title: `
             View this post by
@@ -291,20 +299,95 @@ export default function Post(props) {
               url: window.location.href,
             });
           }}
-           >
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-</svg>
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+          />
+        </svg>
 
-
-
+         {
+          window.location.pathname === "/p/" + props.id ? <div>
+          {
+          
+          
+          bookmarked.includes(api.authStore.model.id) ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#3d85c6"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#3d85c6"
+              className="w-5 h-5 cursor-pointer"
+              onClick={() => {
+                setBookmarked([...bookmarked.filter((id) => id !== api.authStore.model.id)]);
+                api.collection("posts").update(props.id, {
+                  bookmarked: [...bookmarked.filter((id) => id !== api.authStore.model.id)]
+                });
+                // append props.id to user bookmarks
+                let bookmarks = api.authStore.model.bookmarks;
+                api.collection("users").update(api.authStore.model.id, {
+                  bookmarks:   [...bookmarks.filter((id) => id !== props.id)]
+                })
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5 cursor-pointer"
+              onClick={() => {
+                let bookmarks = api.authStore.model.bookmarks;
+                api.collection("users").update(api.authStore.model.id, {
+                  bookmarks: [...bookmarks, props.id]
+                });
+                setBookmarked([...bookmarked, api.authStore.model.id]);
+    
+                api.collection("posts").update(props.id, {
+                  bookmarked:  [...bookmarked, api.authStore.model.id]
+                });
+                 
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+          )}
+          </div> : <></>
+         }
+         
       </div>
-      <div className="flex flex-row font-normal text-slate-400 gap-2 mt-6"
-      
-      >
-      <span> {props.comments.length ?  props.comments.length > 1 ? props.comments.length + " Replies" : props.comments.length + " Reply" :  
-      0 + " Replies"}</span>
-      <span className="text-slate-300">•</span>
-      <span>   {likes.length ?  likes.length > 1 ? likes.length + " Likes" : likes.length + " Like" : "0 Likes"}</span>
+      <div className="flex flex-row font-normal text-slate-400 gap-2 mt-6">
+        <span>
+          {" "}
+          {props.comments.length
+            ? props.comments.length > 1
+              ? props.comments.length + " Replies"
+              : props.comments.length + " Reply"
+            : 0 + " Replies"}
+        </span>
+        <span className="text-slate-300">•</span>
+        <span>
+          {" "}
+          {likes.length
+            ? likes.length > 1
+              ? likes.length + " Likes"
+              : likes.length + " Like"
+            : "0 Likes"}
+        </span>
       </div>
     </div>
   );

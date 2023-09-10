757d55603380afeffe00d8f300b60a70f05edcb4
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-10 10:27:02 -0700

    Update Bottomnav.jsx

--- src/components/Bottomnav.jsx
+++ src/components/Bottomnav.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import sanitizeHtml from "sanitize-html";
 import Modal from "./Modal";
 import { api } from "../react_pages";
-import { doc } from "firebase/firestore";
 
 function handleEmojis(html) {
   let parser = new DOMParser();
@@ -397,7 +396,7 @@ export default function Bottomnav() {
           fontSize: "16px",
         }}
       >
-        <div className=" max-w-screen max-w-screen h-screen w-screen overflow-auto shadow-none fixed top-0 left-0 p-5">
+        <div className=" max-w-screen max-w-screen   w-screen   shadow-none fixed top-0 left-0 p-5">
           <div className="flex flex-row justify-between">
             <div className="flex">
               <img

5c95e467cf7f9074c93bf680d78f92b64587fc89
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-12 08:17:19 -0500

    Update Home.jsx

--- src/react_pages/Home.jsx
+++ src/react_pages/Home.jsx
@@ -59,6 +59,7 @@ export default function Home() {
   useEffect(() => {
     setTotalPages(0)
     setPosts([]);
+    setPage(1)
     fetchPosts(1, pageSelected).then((fetchedPosts) => {
       setPosts(fetchedPosts.items);
       setTotalPages(fetchedPosts.totalPages);

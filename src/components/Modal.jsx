82b43b0d5c47aacdd6eea173bdad87221acb2249
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-14 09:45:09 -0500

    Change modal color based on theme

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal(props) {
               `}
             
             >
-                <form method="dialog" className={`modal-box  stickt rounded-non bg-white  
+                <form method="dialog" className={`modal-box  sticky rounded-non bg-base-100  
                 
                    ${props.height ? props.height : 'h-[100vh]'}
                 `}

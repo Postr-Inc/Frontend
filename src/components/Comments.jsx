bcc9594cb39955d5b312c79d522acb715d2db83a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-22 08:51:20 -0500

    Update Comments.jsx

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -98,7 +98,7 @@ export default function Comment(props) {
        {
             likes && likes.length > 0     &&  likes.includes(props.post.author)
             && props.author !== props.post.author
-            ? <div className=" text-sm flex flex-row gap-2 items-center">
+            ? <div className=" mb-2 text-sm flex flex-row gap-2 items-center">
               
   <svg xmlns="http://www.w3.org/2000/svg" fill="#F13B38" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#F13B38" className="w-4 h-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />

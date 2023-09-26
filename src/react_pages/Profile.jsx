ce45486216812876625adfa456be143a55a373c3
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-26 07:16:26 -0500

    Fix placeholder profile errors

--- src/react_pages/Profile.jsx
+++ src/react_pages/Profile.jsx
@@ -876,10 +876,8 @@ export default function Profile(props) {
                   ) : (
                     <div className="avatar placeholder">
                       <div className="bg-neutral-focus text-neutral-content  border-slate-200 rounded-full w-16">
-                        <span className="text-lg">
-                          {profile.username
-                            ? profile.username.charAt(0).tocapitalize()
-                            : ""}
+                        <span className="text-lg capitalize">
+                          {profile.username}
                         </span>
                       </div>
                     </div>

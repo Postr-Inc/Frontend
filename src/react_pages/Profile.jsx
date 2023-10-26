93a0e2b8cc9bc83175220d93d44b9b55cc215007
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-10-26 11:41:26 -0700

    Update Profile.jsx

--- src/react_pages/Profile.jsx
+++ src/react_pages/Profile.jsx
@@ -82,28 +82,7 @@ export default function Profile(props) {
         return res;
       });
   }
-  useEffect(() => {
-    let theme = localStorage.getItem("theme");
-
-    if (!theme) {
-      localStorage.setItem("theme", "black");
-      document.querySelector("html").setAttribute("data-theme", "black");
-    } else {
-      document.querySelector("html").setAttribute("data-theme", theme);
-      window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .addEventListener("change", (e) => {
-          if (e.matches) {
-            document.querySelector("html").setAttribute("data-theme", "black");
-            localStorage.setItem("theme", "black");
-          } else {
-            document.querySelector("html").setAttribute("data-theme", "light");
-            localStorage.setItem("theme", "light");
-          }
-        });
-    }
-  }, []);
-
+ 
   useEffect(() => {
     api
       .collection("users")

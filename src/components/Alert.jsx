6bf17e7d3d4323f1617025020e8a609f4f40c78c
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-11-13 03:21:25 -0800

    Delete src directory

--- src/components/Alert.jsx
+++
@@ -1,7 +0,0 @@
-export default function Alert(props) {
-    return <div className={`alert alert-${props.type} ${props.className ? props.className : ''}
-    bg-${props.bg ? props.bg : 'bg-base-200'}
-    `}>
-           {props.children}
-       </div>
-}

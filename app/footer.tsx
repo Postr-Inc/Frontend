fe14ca3747d9f5eee0135ca8446a34cd6409c20e
Malik <malikwhitterb@gmail.com>
2023-12-27 20:36:26 -0800

    Update download link in footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(props:any){
         <div className={` ${props.className}  flex hero flex-row flex-wrap w-full   gap-5 mx-auto justify-center   xl:w-[30vw]   lg:w-[50vw]`}>
             &copy; 2023 - Postr-inc
             <button className='btn-sm btn '>Status: <span className='text-success'>Online</span></button>
-            <span className=''>Download Tweeter</span>
+            <span className=''>Download Postr</span>
             <span className=''>Developers</span>
          </div> 
     )

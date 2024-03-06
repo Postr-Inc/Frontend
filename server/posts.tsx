79c1a33c114bc40860d8862d84df2e5d429c9e66
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-03-06 08:14:14 -0600

    cleanup

--- server/posts.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-import { api } from "@/src/api/api";
-
-export default async function fetchPosts(page:number, limit: Number, expand: string[], sort: string, collection: string, filter?: string,) {
-     
-     let res = await api.list({collection: collection, page:page, limit:limit, expand:expand, sort:sort, filter:filter})
-     console.log(res)
-}

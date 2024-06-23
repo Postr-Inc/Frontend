76e1d7fadb67b7f2f7283e54d1fc0a227924d177
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-06-23 15:07:26 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
+const config: Config = { 
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
